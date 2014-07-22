641bb0e8625aab940de67295beb6ae1f94e7bfb5
cassiedoll <cassandradoll@gmail.com>
2014-07-22 13:01:20 -0700

    Add a buildService method
    This should remove a lot of boilerplate associated with calling the
    genomics api from java.
    More parameters may be added before this gets final.

--- src/main/java/com/google/cloud/genomics/utils/GenomicsAuth.java
+++ src/main/java/com/google/cloud/genomics/utils/GenomicsAuth.java
@@ -15,11 +15,26 @@
  */
 package com.google.cloud.genomics.utils;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.java6.auth.oauth2.VerificationCodeReceiver;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.extensions.java6.auth.oauth2.GooglePromptReceiver;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Lists;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.genomics.Genomics;
 
 import java.io.*;
+import java.security.GeneralSecurityException;
+import java.util.List;
 
 public class GenomicsAuth {
   /**
@@ -53,6 +68,73 @@ public class GenomicsAuth {
       throws IOException {
     InputStream inputStream = new FileInputStream(new File(clientSecretsFilename));
     return GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(inputStream));
+  }
+
+  /**
+   * Creates an authorized Genomics service from a client secrets file.
+   * It uses a local server for OAuth and asks for access to the one main Genomics API scope.
+   * See the other buildService method for more options.
+   *
+   * @param applicationName The name of your application. This will be used to store authorization
+   *     credentials and will show up in UserAgent headers. Make sure it is unique to your
+   *     application.
+   * @param clientSecretsFilename The path to the client secrets file that will be loaded.
+   * @return An authorized genomics service object that can call the Google Genomics API.
+   * @throws GeneralSecurityException
+   * @throws IOException
+   */
+  public static Genomics buildService(String applicationName, String clientSecretsFilename)
+      throws GeneralSecurityException, IOException {
+    List<String> scopes = Lists.newArrayList();
+    scopes.add(GENOMICS_SCOPE);
+    return buildService(applicationName, scopes, clientSecretsFilename, false);
+  }
+
+  /**
+   * Creates an authorized Genomics service from a client secrets file.
+   * This uses reasonable defaults for the http transport, json factory, main data store directory.
+   * Use Genomics.Builder for custom setups.
+   *
+   * @param applicationName The name of your application. This will be used to store authorization
+   *     credentials and will show up in UserAgent headers. Make sure it is unique to your
+   *     application.
+   * @param scopes The scopes that will be required by the Google Genomics API calls you are using.
+   *     See the constants in this file for useful scope values.
+   * @param clientSecretsFilename The path to the client secrets file that will be loaded.
+   * @param noLocalServer If true, a command line prompt will be used to ask the user for OAuth
+   *     access. If false, a local server will be started and a browser window will be
+   *     automatically opened for the user. False creates a better user experience, but will not
+   *     work in all environments.
+   * @return An authorized genomics service object that can call the Google Genomics API.
+   * @throws GeneralSecurityException
+   * @throws IOException
+   */
+  public static Genomics buildService(String applicationName, List<String> scopes,
+      String clientSecretsFilename, boolean noLocalServer)
+      throws GeneralSecurityException, IOException {
+
+    GoogleClientSecrets clientSecrets = loadClientSecrets(clientSecretsFilename);
+    NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    File dataStoreDir = new File(System.getProperty("user.home"), ".store/" + applicationName);
+    FileDataStoreFactory dataStoreFactory = new FileDataStoreFactory(dataStoreDir);
+
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        httpTransport, JacksonFactory.getDefaultInstance(), clientSecrets, scopes)
+        .setDataStoreFactory(dataStoreFactory).build();
+    VerificationCodeReceiver receiver = noLocalServer ? new GooglePromptReceiver() :
+        new LocalServerReceiver();
+    final Credential credential = new AuthorizationCodeInstalledApp(flow, receiver)
+        .authorize(applicationName);
+
+    return new Genomics.Builder(httpTransport, JSON_FACTORY, credential)
+        .setApplicationName(applicationName)
+        .setHttpRequestInitializer(new HttpRequestInitializer() {
+          @Override
+          public void initialize(HttpRequest httpRequest) throws IOException {
+            credential.initialize(httpRequest);
+            httpRequest.setReadTimeout(60000); // 60 seconds
+          }
+        }).build();
   }
 
 }
