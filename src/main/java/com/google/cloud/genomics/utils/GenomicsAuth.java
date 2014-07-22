ab18b6faa9b4d7e91fec06ec2e1db150e5f1f1bc
cassiedoll <cassandradoll@gmail.com>
2014-07-22 12:22:59 -0700

    Add additional api scopes, clarify comments

--- src/main/java/com/google/cloud/genomics/utils/GenomicsAuth.java
+++ src/main/java/com/google/cloud/genomics/utils/GenomicsAuth.java
@@ -23,18 +23,30 @@ import java.io.*;
 
 public class GenomicsAuth {
   /**
-   * This is the main Genomics API scope that will be needed by nearly all clients.
+   * This is the main Genomics API scope that is required by nearly all API calls.
    */
   public static final String GENOMICS_SCOPE = "https://www.googleapis.com/auth/genomics";
+
+  /**
+   * The BigQuery API scope is needed to export variants to BigQuery.
+   */
+  public static final String BIGQUERY_SCOPE = "https://www.googleapis.com/auth/bigquery";
+
+  /**
+   * The Devstorage API scope is needed to import from or export to Google Cloud Storage.
+   */
+  public static final String DEVSTORAGE_SCOPE =
+      "https://www.googleapis.com/auth/devstorage.read_write";
 
   private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
   /**
+   * Loads a client secrets file into an GoogleClientSecrets object that can be used to
+   * send authorized requests to the Google Genomics API.
    *
    * @param clientSecretsFilename The path to the client secrets file that will be loaded.
    * @return A GoogleClientSecrets object that can be used with a GoogleAuthorizationCodeFlow.
-   * @throws java.io.FileNotFoundException if the client secrets file doesn't exist
-   *     (this usually should be passed on to the user)
+   * @throws java.io.FileNotFoundException if the client secrets file doesn't exist.
    * @throws java.io.IOException if the file couldn't be parsed into a GoogleClientSecrets object.
    */
   public static GoogleClientSecrets loadClientSecrets(String clientSecretsFilename)
